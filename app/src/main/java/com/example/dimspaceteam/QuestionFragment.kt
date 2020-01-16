5cf13e43d50b7bbf06562979c59415481ac8ad3b
YannickVeron <yveron74@gmail.com>
2020-01-16 21:03:15 +0100

    Add Navigation and questionFragment

---
+++ app/src/main/java/com/example/dimspaceteam/QuestionFragment.kt
@@ -0,0 +1,18 @@
+package com.example.dimspaceteam
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.waiting_room_fragment.*
+
+class QuestionFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.question_fragment,container,false)
+    }
+}
--- app/src/main/java/com/example/dimspaceteam/WaitingRoomFragment.kt
+++ app/src/main/java/com/example/dimspaceteam/WaitingRoomFragment.kt
@@ -1,10 +1,13 @@
 package com.example.dimspaceteam
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.waiting_room_fragment.*
 
 class WaitingRoomFragment : Fragment(){
@@ -13,12 +16,14 @@ class WaitingRoomFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        btnStart.setOnClickListener{Start()}
-        return inflater.inflate(R.layout.waiting_room_fragment,container,false)
+        var view = inflater.inflate(R.layout.waiting_room_fragment,container,false);
+        var startQuestionBtn = view.findViewById<Button>(R.id.btnStart)
+        startQuestionBtn.setOnClickListener {Start(view)}
+        return view
     }
 
-    fun Start(){
-
+    fun Start(view: View?){
+        view?.findNavController()?.navigate(R.id.questionFragment)
     }
 }
 
