45c0add0f0c52395fad2472a22c06d5fb6c731ff
martindavid <martin.david74140@gmail.com>
2020-01-21 10:35:15 +0100

    Add class GameWin

--- app/src/main/java/com/example/dimspaceteam/GameWin.kt
+++ app/src/main/java/com/example/dimspaceteam/GameWin.kt
@@ -1,100 +1,65 @@
 package com.example.dimspaceteam
 
 import android.content.Context
-import android.net.Uri
+import android.content.Intent
+import android.graphics.Movie
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Switch
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import com.example.dimspaceteam.model.Event
+import com.example.dimspaceteam.model.EventType
+import com.example.dimspaceteam.model.UIElement
+import com.example.dimspaceteam.model.UIType
+import com.github.nisrulz.sensey.Sensey
+import com.github.nisrulz.sensey.ShakeDetector
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.android.synthetic.main.fragment_game_over.*
+import kotlinx.android.synthetic.main.question_fragment.*
+import kotlinx.android.synthetic.main.waiting_room_fragment.*
+import okhttp3.WebSocket
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [GameWin.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [GameWin.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GameWin : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel: EventViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_win, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameWin.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameWin().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return EventViewModel(WebSocketClient) as T
             }
+        }
+        var view = inflater.inflate(R.layout.fragment_game_win,container,false)
+
+        viewModel = ViewModelProviders.of(this,factory).get(EventViewModel::class.java)
+        viewModel.getCurrentEvent().observe(this, Observer{})
+
+        view.findViewById<Button>(R.id.btnRestart).setOnClickListener{
+            view?.findNavController()?.navigate(R.id.waitingRoomFragment)
+        }
+        view.findViewById<Button>(R.id.btnLeaveGame).setOnClickListener {
+            viewModel.webSocketClient.webSocket?.close(1000,"Game Complete")
+            val intent = Intent(context,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
